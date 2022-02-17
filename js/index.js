3630eb17827317fa89ac7afbd3b1680e009ec52f
Hutasako <ejjungao@yahoo.com>
2022-02-17 02:20:28 -0500

    some comments

--- js/index.js
+++ js/index.js
@@ -7,34 +7,37 @@ document.addEventListener("DOMContentLoaded", function() {
     function doThing(data = null) {
         // console.log(data);
         fetch("https://url-shortener-service.p.rapidapi.com/shorten", {
-	"method": "POST",
-	"headers": {
-		"content-type": 'application/json',
-		"x-rapidapi-host": "url-shortener-service.p.rapidapi.com",
-		"x-rapidapi-key": "d065a6d56cmsh177414c74052be8p1726dfjsn7ba97c54ee16"
-	},
-    "body": data,
-    })
-    .then((response) => response.json())
-    .then(response => {
-        // console.log(response);
-        // console.log(response.result_url);
-        resultField.value = response.result_url;      
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        "method": "POST",
+        "headers": {
+            "content-type": 'application/json',
+            "x-rapidapi-host": "url-shortener-service.p.rapidapi.com",
+            "x-rapidapi-key": "d065a6d56cmsh177414c74052be8p1726dfjsn7ba97c54ee16"
+        },
+        "body": data,
+        })
+        .then((response) => response.json())
+        .then(response => {
+            // console.log(response);
+            // console.log(response.result_url);
+            resultField.value = response.result_url;      
+        })
+        .catch(err => {
+            console.error(err);
+        });
     }
 
     urlForm.addEventListener("submit", function(e){
         e.preventDefault();
+        // Reset the copy button whenever a new link is requested
         copyButton.classList.remove("button-success");
         copyButton.innerText = "Copy";
+
         const urlFormData = new FormData(urlForm);
         let jurl = JSON.stringify(Object.fromEntries(urlFormData.entries()));
         doThing(jurl);
     })
 
+    // Clipboard function (copy)
     copyButton.addEventListener("click", function(e) {
         navigator.clipboard.writeText(resultField.value).then(function(e){
             copyButton.classList.add("button-success");
